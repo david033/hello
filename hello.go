bfcdd608f65739dbfd293d1ad3071fc42a97fea4
David Szegedi <david.szegedi93@gmail.com>
2020-02-03 19:50:13 +0100

    Morestrings package.

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/david033/hello/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
---
+++ morestrings/go.mod
@@ -0,0 +1,3 @@
+module github.com/david033/hello/morestrings
+
+go 1.13
